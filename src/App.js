d8a80f59ca827d1b905bddae6dadc7c2ec9624b1
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-10 22:28:16 +0100

    refactor: add components and router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
+import Place from './components/Place';
 
 function App() {
   return (
     <div className="App">
-      <h1>meta</h1>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Section />} />
+          <Route path='/place' element={<Place />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
