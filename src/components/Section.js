b61c05e9a7b254671c909d0d6e851e8232ebfc3f
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-11 08:14:25 +0100

    style: style popup

--- src/components/Section.js
+++ src/components/Section.js
@@ -7,18 +7,19 @@ const Section = () => {
         <div className="connect">
           <div>
             <h3>Connect Wallet</h3>
-            <img src="/assets/close.svg" alt="close" />
+            <img src="/assets/close.svg" alt="close" className='close' />
           </div>
+            <div className="line"></div>
           <div>
             <p>Choose your preferred wallet:</p>
-            <div className="wallwet">
+            <div className="wallet">
               <div>
                 <img src="/assets/meta-wallet.svg" alt="metamask" />
                 <h3>Metamask</h3>
               </div>
               <img src="/assets/arrow.svg" alt="arrow" />
             </div>
-            <div className="wallwet">
+            <div className="wallet">
               <div>
                 <img src="/assets/connect-wallet.svg" alt="walletConnect" />
                 <h3>WalletConnect</h3>
