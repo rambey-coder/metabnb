522bda4015eff7afc3e675326b3bfedd6bd664ac
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-11 12:09:52 +0100

    feat: add inspiration component

--- src/components/Section.js
+++ src/components/Section.js
@@ -6,6 +6,7 @@ const Section = () => {
     <div>
       <Modal />
       <div className="container">
+        {/* HERRO-SECTION */}
         <div className="section">
           <div className="txt-sec">
             <h1>
@@ -30,12 +31,121 @@ const Section = () => {
           </div>
         </div>
       </div>
+      {/*  */}
 
+      {/* LOGO-LABEL */}
       <div className="icon-label">
         <div className="container icon-container">
           <img src="/assets/mb-token.svg" alt="token" />
           <img src="/assets/metamask.svg" alt="metamask" />
           <img src="/assets/opeansea.svg" alt="opensea" />
+        </div>
+      </div>
+      {/*  */}
+
+      {/* INSPIRATIONS */}
+      <div className="container">
+        <div className="inspiration">
+          <h1>Inspiration for your next adventure</h1>
+
+          <div className="card-container">
+            <div className="card">
+              <img src="/assets/desert-king9.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king2.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king3.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king4.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king5.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king6.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king7.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+            <div className="card">
+              <img src="/assets/desert-king8.png" alt="desertKing" />
+              <span>
+                <p>Desert King</p>
+                <p>1MBT per night</p>
+              </span>
+              <span>
+              <p>2345km away</p>
+              <p>available for 2weeks stay</p>
+              </span>
+                <img src="/assets/star.svg" alt="rating" />
+            </div>
+          </div>
         </div>
       </div>
     </div>
