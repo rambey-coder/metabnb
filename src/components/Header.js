508672069bb118b59116327dd0a01770ddb0671b
rambey-coder <olayiwolaagboolaakanji@gmail.com>
2022-11-11 07:15:59 +0100

    style: styled the navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,31 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="container">
+      <div className="nav">
+       <Link to='/'>
+        <img src="/assets/meta-logo.svg" alt="logo" />
+       </Link>
+
+        <div className="lists">
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+
+          <li>
+            <Link to='/place'>Place to stay</Link>
+          </li>
+
+          <li>NFTs</li>
+
+          <li>Community</li>
+        </div>
+
+        <button>Connect Wallet</button>
+      </div>
+    </div>
   )
 }
 
